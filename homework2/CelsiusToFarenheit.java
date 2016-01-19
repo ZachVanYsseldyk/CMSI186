e9a62719a0b798b153f4df52edd899b64d260d8b
ZachVanYsseldyk <zvanysse@lion.lmu.edu>
2016-01-19 12:28:05 -0800

    COMMIIT!!

---
+++ homework2/CelsiusToFarenheit.java
@@ -0,0 +1,8 @@
+class CelsiusToFarenheit {
+	public static void main(String args[]) {
+		float celsius = 24.3f;
+		double conversion = 1.8 * celsius + 32;
+		System.out.println("24.3 celsius into Farenheit? Glad you asked. According to my amazing coding and mathematic skills, 24.3 celsius in Farenheit is "
+			 + (conversion) + "F.");
+	}
+}
